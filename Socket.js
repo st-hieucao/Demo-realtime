b6e750b7334e4762bdf2f4ea3be1239893d16244
CaoKhaHieu <caokhahieu9@gmail.com>
2024-01-12 13:42:26 +0700

    init

---
+++ Socket.js
@@ -0,0 +1,45 @@
+import { Server } from "socket.io";
+import { UAParser } from 'ua-parser-js'
+
+const devices = [];
+
+export const ConnectSocket = (server) => {
+  const io = new Server(server, {
+    cors: {
+      origin: [
+        "http://localhost:8080",
+        "https://phaata.info",
+        "https://user.phaata.info",
+        "https://phaata.com",
+        "https://user.phaata.com",
+        "http://localhost:5173",
+      ],
+      methods: ["GET", "POST"],
+      allowedHeaders: ["my-custom-header"],
+      credentials: true,
+    },
+  });
+
+  io.on("connection", (socket) => {
+    const userAgent = socket.handshake.headers['user-agent'];
+    let parser = new UAParser(userAgent); // you need to pass the user-agent for nodejs
+    let parserResults = parser.getResult();
+
+    socket.on('watch-video', (agent) => {
+      if (!devices.includes(agent)) {
+        devices.push(agent);
+        io.emit('total-device', devices.length);
+      }
+    });
+
+    io.emit('total-device', devices.length);
+
+    socket.on('disconnect', function(){
+      const index = devices.indexOf(parserResults.ua);
+      if (index > -1) {
+        devices.splice(index, 1);
+        io.emit('total-device', devices.length);
+      }
+    });
+  });
+};
---
+++ index.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import { createServer } from "http";
+import { ConnectSocket } from './Socket';
+
+export const connectDb = async () => {
+  const url = 'mongodb+srv://CaoKhaHieu:CaoKhaHieu@cluster0.5sfoj.mongodb.net/DemoRealtime?retryWrites=true&w=majority';
+  try {
+    mongoose.set("strictQuery", false);
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    console.log("Connected to DB");
+  } catch (error) {
+    console.log('Error when connect to DB', error);
+  }
+};
+
+const app = express();
+const server = createServer(app);
+const PORT = 4000;
+
+ConnectSocket(server);
+connectDb();
+
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
+server.listen(PORT, () => {
+  console.log(`app run on port ${PORT}`);
+});
+
